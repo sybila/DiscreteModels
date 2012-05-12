2fb316ee6abdf47834dc8e08a6547bd3c13f8b7d
Adam Streck <adam.streck@gmail.com>
2012-05-12 18:18:08 +0200

    Script fixed.

--- Bacteriophage/Benchmark/distribution.sh
+++ Bacteriophage/Benchmark/distribution.sh
@@ -1,10 +1,10 @@
-#Run distribution benchmark
+#run distribution benchmark
 
 a=1
 while [ "$a" -lt 9 ]
 do
 	b=1
-	while [ "$b" -lt "$a" ]
+	while [ "$b" -le "$a" ]
 	do
 		../../../Parsybone/GCCProject/Parsybone -st < model.dbm -D "$b" "$a" >> output.out
 		b=`expr $b + 1`
