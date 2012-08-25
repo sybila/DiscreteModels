e8c4d7cff6d4be6dad5f6b255217cbc20744ac19
adam.streck@gmail.com <adam.streck@gmail.com>
2012-08-26 01:59:35 +0200

    Testing script extended.

--- Test/test.sh
+++ Test/test.sh
@@ -1,3 +1,4 @@
+#tests that are supposed to raise exception
 for i in Error*
 do
 	echo "$i"
@@ -7,6 +8,7 @@ do
 		echo "	did not returned error value."
 	fi
 done
+#tests that are supposed to work out
 for i in Test*
 do
 	echo "$i"
@@ -16,3 +18,24 @@ do
 		echo "	returned error value."
 	fi
 done
+#test distribution
+echo "Test distribution."
+./Parsybone.exe Test_distribute.dbm -d 1 1 -f all.testout
+VAR1=`wc -l all.testout`
+set -- $VAR1
+VAR1=$1
+VAR2=0
+for i in {1..10}
+do
+	./Parsybone.exe Test_distribute.dbm -d $i 10 -f $i.testout
+	TEMP=`wc -l $i.testout`
+	set -- $TEMP
+	TEMP=$1
+	VAR2=$(($VAR2 + $TEMP))
+done
+if [ $VAR1 != $VAR2 ]
+then
+	echo "	distribution failed"
+	echo "	value "$VAR1" is not equal sum" $VAR2 
+fi
+rm *.testout
