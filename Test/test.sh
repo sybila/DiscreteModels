aa5585297a8932dc41c7adbdc3b95c7355ad94a3
adam.streck@gmail.com <adam.streck@gmail.com>
2012-08-26 18:04:40 +0200

    New tests added.

--- Test/test.sh
+++ Test/test.sh
@@ -1,8 +1,10 @@
+#must have executable as a first parameter
+PARSYBONE=$1
 #tests that are supposed to raise exception
 for i in Error*
 do
 	echo "$i"
-	./Parsybone.exe $i 2>/dev/null 1>/dev/null
+	$PARSYBONE $i 2>/dev/null 1>/dev/null
 	if [ $? == 0 ]
 	then
 		echo "	did not returned error value."
@@ -12,22 +14,22 @@ done
 for i in Test*
 do
 	echo "$i"
-	./Parsybone.exe $i 2>/dev/null 1>/dev/null
+	$PARSYBONE $i 2>/dev/null 1>/dev/null
 	if [ $? != 0 ]
 	then
 		echo "	returned error value."
 	fi
 done
 #test distribution
-echo "Test distribution."
-./Parsybone.exe Test_distribute.dbm -d 1 1 -f all.testout
+echo "Checking correctness of distribution."
+$PARSYBONE Check_distribution.dbm -d 1 1 -f all.testout
 VAR1=`wc -l all.testout`
 set -- $VAR1
 VAR1=$1
 VAR2=0
 for i in {1..10}
 do
-	./Parsybone.exe Test_distribute.dbm -d $i 10 -f $i.testout
+	$PARSYBONE Check_distribution.dbm -d $i 10 -f $i.testout
 	TEMP=`wc -l $i.testout`
 	set -- $TEMP
 	TEMP=$1
