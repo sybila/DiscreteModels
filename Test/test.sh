08e0725469658faf9097601567a741b712b549a5
adam.streck@gmail.com <adam.streck@gmail.com>
2012-08-26 18:33:07 +0200

    Testing script extended.

--- Test/test.sh
+++ Test/test.sh
@@ -41,3 +41,17 @@ then
 	echo "	value "$VAR1" is not equal sum" $VAR2 
 fi
 rm *.testout
+#test correctnes
+echo "Checking correctness of results1."
+$PARSYBONE Check_results.dbm -Wr -f check.out
+if [ `cat check.out` != "0:(0,0,2,1,1,2,1,0):6:0.25:{(0,0;0)>(0,1;1),(0,1;1)>(1,1;1),(1,1;1)>(2,1;2),(1,1;1)>(1,2;2),(2,1;2)>(2,2;2),(2,1;3)>(1,1;3),(1,2;2)>(2,2;2),(1,2;3)>(1,1;3),(2,2;2)>(2,1;3),(2,2;2)>(1,2;3)}" ]
+then
+	echo "	results differ."
+fi
+echo "Checking correctness of results2."
+$PARSYBONE Check_results2.dbm -wr -f check.out
+if [ `head -1 check.out` != "0:(1,2,2,0,2,2,2,2,1,0,0,1,0,0,1,0,3,0,2,0,1,0,0,0):8:0.09722222222222221:{0>13,0>25,0>97,13>37,13>109,25>37,25>121,34>10,37>133,97>101,97>109,97>121,101>113,101>125,106>10,109>113,109>133,113>137,121>133,125>137,129>34,129>106,133>137,137>141,141>129}" ]
+then
+	echo "	results differ."
+fi
+rm check.out
